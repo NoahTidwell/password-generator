9c8d2f24a5ae505b1bac27111f980159949ef473
Noah Tidwell <ntidwell34@yahoo.com>
2021-09-08 22:30:01 -0500

    password display, broken needs fix.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ var num = "1234567890"
 var symb = "~!@#$%^&*()-_=+[]{};:',./<>?"
 var chars = "";
 
+// Reset Password
+function reset() {
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -22,29 +28,29 @@ generateBtn.addEventListener("click", writePassword);
 
 // Add special characters to string
 
-function addSpecial(x) {
-  if (x === true) {
+function addSpecial(specialCharacters) {
+  if (specialCharacters === true) {
     chars = chars.concat(symb)
   }
 }
 
 // Add number character to string
-function addNumber(x) {
-if (x === true) {
+function addNumber(numberCharacters) {
+if (numberCharacters === true) {
   chars = chars.concat(num)
   }
 }
 
 // Add lowercase letters to string
-function addLower(x) {
-  if (x === true) {
+function addLower(lowercase) {
+  if (lowercase === true) {
     chars = chars.concat(lowchar)
   }
 }
 
 // Add uppercase letters to string
-function addUpper(x) {
-  if (x === true) {
+function addUpper(uppercase) {
+  if (uppercase === true) {
     chars = chars.concat(upchar)
   }
 }
@@ -52,10 +58,10 @@ function addUpper(x) {
 // Generate password Logic based on character Input
 function generatePassword() {
   var pwdLength = prompt("How many characters would you like in your password? Choose a number between 8 and 24.");
-  if (pwdLength > 24 || pwdLength < 8) {
+  if (pwdLength > 128 || pwdLength < 8) {
     alert("Invalid input. Please enter a number between 8 and 24")
   }
-  else if (pwdLength <= 24 || pwdLength >= 8) {
+  else if (pwdLength <= 128 || pwdLength >= 8) {
     var specialCharacters = confirm("Include special characters in your password?")
 
     var numberCharacters = confirm("Include number characters in your password?")
@@ -69,8 +75,8 @@ function generatePassword() {
     }
 
     for (i=1; i<pwdLength; i++) {
-      var c = Math.floor(Math.random()*chars.length + 1);
-      password += chars.charAt(c)
+      var k = Math.floor(Math.random()*chars.length + 1);
+      password += chars.charAt(k)
     }
     return password;
   }
