f7cbbf1f3f80a29de10b448c967a2aaf32eeacf7
Noah Tidwell <ntidwell34@yahoo.com>
2021-09-12 10:05:11 -0500

    refactor code, another failed attempt at fixing display

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password")
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   pwd = "";
@@ -15,10 +15,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
  //Password values
- var symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ':', ';', ',', '<', '>', '?']
- var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
- var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
- var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ var symbols = "~!@#$%^&*()-_=+:;,<>?"
+ var numbers = "0123456789"
+ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ var lowercase = "abcdefghijklmnopqrstuvwxyz"
  var pwd = "";
 
  // Add symbol character to pwd string
@@ -59,36 +59,35 @@ function generatePassword() {
   else if (pwdLength >= 8 && pwdLength <= 128) {
 
     var symConfirm = confirm("Include symbols in your password?")
-    var numConfirm = confirm("Include numbers in your password?")
-    var upConfirm = confirm("Include uppercase letters in your password?")
-    var lowConfirm = confirm("Include lowercase letters in your password?")
-
-    
-    if (symConfirm === false && numConfirm === false && upConfirm === false && lowConfirm === false) {
-      alert("Please choose at leaset one character type to proceed.");
-
-      symConfirm = confirm("Include symbols in your password?")
       addSymbol(symbols)
       console.log(pwd)
 
-      numConfirm = confirm("Include numbers in your password?")
+    var numConfirm = confirm("Include numbers in your password?")
       addNumber(numbers)
       console.log(pwd)
 
-      upConfirm = confirm("Include uppercase letters in your password?")
+    var upConfirm = confirm("Include uppercase letters in your password?")
       addUpper(uppercase)
       console.log(pwd)
 
-      lowConfirm = confirm("Include lowercase letters in your password?")
+    var lowConfirm = confirm("Include lowercase letters in your password?")
       addLower(lowercase)
       console.log(pwd)
-    }
-  }
+  
     
-    for (let i = 1; i < pwdLength, i++;) {
-      var chars = Math.floor(Math.random() * pwd.length + 1);
-      password += pwd.charAt(chars)
+    if (symConfirm === false && numConfirm === false && upConfirm === false && lowConfirm === false) {
+      alert("Please choose at leaset one character type to proceed.");
+        symConfirm = confirm("Include symbols in your password?")
+        numConfirm = confirm("Include numbers in your password?")
+        upConfirm = confirm("Include uppercase letters in your password?")
+        lowConfirm = confirm("Include lowercase letters in your password?")
+      }
+    
+    for (i = 1; i <= pwdLength; i++) {
+      var z = Math.floor(Math.random() * pwd.length + 1);
+      output += pwd.charAt(z)
     }
- return password;
+ return output;
+    }
 
-};
+}
